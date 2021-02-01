9ff084b83a55aa378f0a8e8b1f29ddf5b06d54cd
fiixed <fiixed@gmail.com>
2021-02-01 14:37:10 -0500

    finish contact

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Home from './components/Home';
 import Resume from './components/Resume';
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route exact path='/' component={Home} />
       <Route path='/resume' component={Resume} />
       <Route path='/projects' component={Projects} />
+      <Route path='/contact' component={Contact} />
     </>
   );
 }
